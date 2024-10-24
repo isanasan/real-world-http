e40ea342c3552320b4a511b64f8a79dc87587e25
isanasan <yfcgpsebp@gmail.com>
2024-10-24 09:30:14 +0900

    p.84 3.4

--- simpleget/main.go
+++ simpleget/main.go
@@ -18,4 +18,8 @@ func main()  {
 		panic(err)
 	}
 	log.Println(string(body))
+	log.Println("Status:", resp.Status)
+	log.Println("StatusCode:", resp.StatusCode)
+	log.Println("Headers:", resp.Header)
+	log.Println("Content-Length:", resp.Header.Get("Content-Length"))
 }
