91f5db69e9894428440bbd043c24c4d9705f2060
isanasan <yfcgpsebp@gmail.com>
2024-11-12 10:30:45 +0900

    p.88 3.8

--- simpleget/main.go
+++ simpleget/main.go
@@ -1,29 +1,20 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
+	"os"
 )
 
 func main()  {
-	values := url.Values{
-		"test": {"value"},
-	}
-	resp, err := http.PostForm("http://localhost:18888", values)
+	file, err := os.Open("main.go")
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
+	resp, err := http.Post("http://localhost:18888", "text/plain", file)
+	if err != nil {
+		panic(err)
+	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	log.Println(string(body))
 	log.Println("Status:", resp.Status)
-	log.Println("StatusCode:", resp.StatusCode)
-	log.Println("Headers:", resp.Header)
-	log.Println("Content-Length:", resp.Header.Get("Content-Length"))
 }
